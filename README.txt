ce1a4fabd30a75b342457b459824cf8469b2ca09
Prince-of-Galar <61099822+Prince-of-Galar@users.noreply.github.com>
2020-02-15 19:46:47 -0600

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,20 @@
+~*~*~*~*~*~*~*~*~*~*~*~
+  C H A T T Y  B O T
+~*~*~*~*~*~*~*~*~*~*~*~
+
+Written for the r/PokemonMaxRaids Discord server by Ymir | Prince_of_Galar
+
+
+ Instructions for Use
+~~~~~~~~~~~~~~~~~~~~~~~
+Note: User must have the Max Host role in order to interact with this bot
+
+To Pin a Message:
+Add the :pushpin: reaction emoji to any message in an appropriate channel.
+
+To Unpin a Message:
+Remove a :pushpin: reaction emoji from any message in an appropriate channel. If you are not the original pinner, you must first add the specified reaction emoji, then remove it.
+
+ Conclusion
+~~~~~~~~~~~~~
+Thank you for using ChattyBot!
---
+++ bot.py
@@ -0,0 +1,28 @@
+#Chatty Bot
+#Created for the r/PokemonMaxRaids discord server
+#Authored by Ymir | Prince_of_Galar
+
+#setup
+import discord
+import asyncio
+from discord.utils import get
+
+client = discord.Client() 
+
+#If a user with the Max Host role adds a :pushpin: (📌) reaction to a message, the message will be pinned
+@client.event
+async def on_reaction_add(reaction, user):
+    if get(user.roles, name = "Max Host") and reaction.emoji == '📌':
+        await reaction.message.pin()
+
+#If a user with the Max Host role removes a :pushpin: (📌) reaction from the message, the message will be unpinned
+@client.event
+async def on_reaction_remove(reaction, user):
+    if  get(user.roles, name = "Max Host") and reaction.emoji == '📌':
+            await reaction.message.unpin()
+
+#runs the app
+if __name__ == '__main__':
+    client.run('Njc4MjYwNDEyNTQxODI5MTMw.XkgV2Q.KC7ciXL3ymkKfio-dF42dFSut3w')
+
+#Last published February 15th, 2020
