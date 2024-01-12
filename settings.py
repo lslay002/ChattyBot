7229c55e114723abf35908c2e39dbbf7b71162d4
lslay002 <90275600+lslay002@users.noreply.github.com>
2024-01-11 22:04:19 -0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,12 +3,12 @@
 # Automute Settings
 autoCallChn = 672590825603203072
 autoCallRole = 672590994755158029
-modRole = 645013071827566632
-ruleChn = 646053713907023889
+modRole = 1171692248505790545
+ruleChn = 1171693492792209488
 
 # Ban Settings
-guildName = "the Pokemon Max Raids Discord"
-appealMes = "You may appeal your ban here: <https://old.reddit.com/message/compose?to=%2Fr%2Fpokemonmaxraids>"
+guildName = "LG Unite"
+appealMes = "Questions about the ban? Join <SERVERINVITE> and message me. I'll get back to you as soon as I can. I can't hear you unless you join that server though, so do that first."
 
 # Setting for where to moniter length
 announcechn = 666388961303855115
