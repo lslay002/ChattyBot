6616f977a7243a5c82a6d6f6b8eecd49eb22850f
lslay002 <90275600+lslay002@users.noreply.github.com>
2024-03-12 16:59:23 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,18 +3,18 @@
 # Automute Settings
 autoCallChn = 672590825603203072
 autoCallRole = 672590994755158029
-modRole = 1171692248505790545
+modRole = 1088355847039623218
 ruleChn = 1171693492792209488
 
 # Ban Settings
-guildName = "LG Unite"
+guildName = "Ogygia"
 appealMes = "Questions about the ban? Join <SERVERINVITE> and message me. I'll get back to you as soon as I can. I can't hear you unless you join that server though, so do that first."
 
 # Setting for where to moniter length
 announcechn = 666388961303855115
 
 # Settings for ;clear
-reactemote = 1193497931261685840
+reactemote = 1193481884034941028
 
 # Settings that determine behavior
 
